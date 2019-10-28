99ef97b80cf13233279021ff7f60a4f5310d05a9
Vladislav <vgrechannik@gmail.com>
2019-10-29 02:54:19 +0300

    Ya zakonchil
    Voobshe ya dumal v dva dnya sdelat', no pochemuto sdelal v odin den

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ fathername.addEventListener("input", function (event) {
         fathername.setCustomValidity("Ваше отчество должно начинаться с большой буквы и состоять из букв английского или русского языка");
     }
   });
+let email=document.getElementById("email");
+email.addEventListener("input", function (event){
+    if (!email.validity.patternMismatch) {
+        email.setCustomValidity("");
+    } else {
+        email.setCustomValidity("Вы должны закончить адрес почты написанием доменного имени. Пример:...@mail.ru");
+    }
+  });
+let password=document.getElementById("password");
+password.addEventListener("input", function (event){
+    if (!password.validity.patternMismatch) {
+        password.setCustomValidity("");
+    } else {
+        password.setCustomValidity("Пароль должен содержать как минимум цифры, символы нижнего и верхнего регистра.");
+    }
+});
+document.getElementById("form").onsubmit = function (evt) {
+    localStorage.clear();
+    evt.preventDefault();
+    console.log(evt);
+    let object = {};
+    for (let i = 0; i < 7; i++) {
+        object[evt.target[i].name] = evt.target[i].value;
+    }
+    localStorage.setItem("data", JSON.stringify(object));
+    
+}
+window.onload = function(){
+    if(localStorage.getItem("data")){
+        let object = JSON.parse(localStorage.getItem("data"));
+        let arr = ["name","secondname","fathername","email","password","date","gender"];
+        arr.forEach(element => {
+            if(document.getElementById(element))
+            document.getElementById(element).value = object[element];
+        });
+        
+        
+    }
+}
