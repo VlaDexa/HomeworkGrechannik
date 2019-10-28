bb5ab2b0f8c8502e169d694a99b0484812d40d1e
Vladislav <vgrechannik@gmail.com>
2019-10-28 21:33:51 +0300

    Restart
    To Windows

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-function erorrer(){
-    document.getElementById("name").onkeydown = function (evt) {
-        let regEx = /[A-Z]\w/;
-        if (!regEx.test(evt.target.value)) {
-            console.log(evt);
-            console.log(evt.target.validity.valid);
-            document.getElementById("name").validity.valid = false;
-            document
-                .getElementById("error")
-                .innerHTML = `Your name should be like Sergei`;
-        } else {
-            document.getElementById("name").validity.valid = true;
-            document
-                .getElementById("error")
-                .innerHTML = ""
-            console.log(evt.target.validity.valid);
-        }
+document.getElementById("name").onsubmit = function validateComments(input) {
+    let regEx = /^[A-ZА-Я]\S*/;
+    if (!regEx.test(evt.target.value)) {
+        console.log(evt);
+        console.log(evt.target.validity.valid);
+        document.getElementById("name").validity.valid = false;
+        input.setCustomValidity("Ваше имя должно начинаться с большой буквы и состоять из букв английского или русского языка");
+    } else {
+        document.getElementById("name").validity.valid = true;
+        document
+            .getElementById("error")
+            .innerHTML = ""
+        console.log(evt.target.validity.valid);
+    }
+}
+document.getElementById("secondname").onsubmit = function (evt) { 
+    if (!regEx.test(evt.target.value)) {
+        console.log(evt);
+        console.log(evt.target.validity.valid);
+        document.getElementById("secondname").validity.valid = false;
+        input.setCustomValidity("Ваша фамилия должна начинаться с большой буквы и состоять из букв английского или русского языка");
+    } else {
+        document.getElementById("secondname").validity.valid = true;
+        document
+            .getElementById("2error")
+            .innerHTML = ""
+        console.log(evt.target.validity.valid);
     }
 }
